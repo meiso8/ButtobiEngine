68daad12f214ba712b68f146dcfd8a9b15bd42f2
あ <k024g0101@g.neec.ac.jp>
2025-10-10 15:19:42 +0900

    mainの修正

--- project/Source/main.cpp
+++ project/Source/main.cpp
@@ -8,18 +8,25 @@
 //Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
+    // ==============================================//↓基本いじらない↓//============================================
     //エンジンの生成
     MyEngine* myEngine = MyEngine::GetInstance();
     myEngine->Create(L"2102", WIN_WIDTH, WIN_HEIGHT);
-
     //音声の読み込み
     Sound::LoadAllSound();
     //テスクチャ読み込み
     Texture::LoadAllTexture();
     //モデル読み込み
     ModelManager::LoadAllModel();
+    // ==============================================//↑基本いじらない↑//============================================
+
     //画面の色
     Vector4 screenColor = { 0.6f,0.6f,0.6f,1.0f };
+
+    // =============================================
+    // シーンの生成
+    // =============================================
+
     //サンプルシーンの生成
     std::unique_ptr<SampleScene> sampleScene = std::make_unique<SampleScene>();
     // ゲームシーンのインスタンスの取得
@@ -48,7 +55,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 #ifdef _DEBUG
         //デバック用
         currentScene->Debug();
-        DebugUI::CheckColor(screenColor,"screenColor");
+        DebugUI::CheckColor(screenColor, "screenColor");
 #endif // _DEBUG
 
         //シーンの更新処理
