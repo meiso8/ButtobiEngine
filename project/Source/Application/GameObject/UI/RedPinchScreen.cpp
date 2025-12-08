0f5000c8f11692045aa0229e82161d86f078ca95
JamesDaionkai524 <k024g0105@g.neec.ac.jp>
2025-12-08 16:55:31 +0900

    HPが1になった際の点滅演出を追加。

--- project/Source/Application/GameObject/UI/RedPinchScreen.cpp
+++ project/Source/Application/GameObject/UI/RedPinchScreen.cpp
@@ -5,55 +5,78 @@
 #include"DebugUI.h"
 RedPinchScreen::RedPinchScreen()
 {
-    sprite_ = std::make_unique<Sprite>();
-    sprite_->Create(Texture::BLOOM, { 0.0f,0.0f }, { 1.0f,0.0f,0.0f,1.0f });
-    sprite_->SetSize({ static_cast<float>(Window::GetClientWidth()),
-        static_cast<float>(Window::GetClientHeight()) });
+	sprite_ = std::make_unique<Sprite>();
+	sprite_->Create(Texture::BLOOM, { 0.0f,0.0f }, { 1.0f,0.0f,0.0f,1.0f });
+	sprite_->SetSize({ static_cast<float>(Window::GetClientWidth()),
+		static_cast<float>(Window::GetClientHeight()) });
 }
 
 void RedPinchScreen::Init()
 {
-    sprite_->SetColor({ 1.0f,0.0f,0.0f,0.0f });
-    timer_ = 0.0f;
+	sprite_->SetColor({ 1.0f,0.0f,0.0f,0.0f });
+	timer_ = 0.0f;
+	flashTimer_ = 0.0f;
+	isFlashing_ = true;
 }
 
 void RedPinchScreen::Update()
 {
-    if (*isHitPtr_) {
-        timer_ = 0.0f;
-    }
+	if (*isHitPtr_) {
+		timer_ = 0.0f;
+	}
 
-    TimerUpdate();
+	TimerUpdate();
 
-    float intensity = Easing::EaseOutExpo(1.0f, 0.0f, timer_); // 1→0 に減衰
+	float intensity = Easing::EaseOutExpo(1.0f, 0.0f, timer_); // 1→0 に減衰
 
-    if (hpPtr_ && hpPtr_->hp <= 20.0f) {
-        sprite_->SetColor({ 1.0f - intensity, 0.0f, 0.0f, timer_ * 0.4f }); // 半透明赤
-    } else {
-        // 赤の強さとアルファをイージングで制御
-        sprite_->SetColor({ intensity, 0.0f, 0.0f, intensity * 0.5f }); // 半透明赤
-    }
+	if (hpPtr_ && hpPtr_->hp <= 20.0f) {
 
-    DebugUI::CheckSprite(*sprite_, "redSprite");
+		if (isFlashing_) {
+			flashTimer_ += InverseFPS;
+			if (flashTimer_ >= kFlashTime_) {
+				flashTimer_ = kFlashTime_;
+				isFlashing_ = false;
+			}
+
+		} else {
+			flashTimer_ -= InverseFPS;
+			if (flashTimer_ <= 0) {
+				flashTimer_ = 0;
+				isFlashing_ = true;
+			}
+
+		}
+
+		float LerpAlpha = Lerp(kFlashMinAlpha_, kFlashMaxAlpha_, flashTimer_);
+
+		sprite_->SetColor({ 1.0f - intensity, 0.0f, 0.0f, LerpAlpha }); // 半透明赤
+
+		//sprite_->SetColor({ 1.0f - intensity, 0.0f, 0.0f, timer_ * 0.4f }); // 半透明赤
+	} else {
+		// 赤の強さとアルファをイージングで制御
+		sprite_->SetColor({ intensity, 0.0f, 0.0f, intensity * 0.5f }); // 半透明赤
+	}
+
+	DebugUI::CheckSprite(*sprite_, "redSprite");
 
 }
 
 void RedPinchScreen::TimerUpdate()
 {
-    if (timer_ == 1.0f) {
-        return;
-    }
+	if (timer_ == 1.0f) {
+		return;
+	}
 
-    timer_ += InverseFPS;
+	timer_ += InverseFPS;
 
-    if (timer_ > 1.0f) {
-        timer_ = 1.0f;
-    }
+	if (timer_ > 1.0f) {
+		timer_ = 1.0f;
+	}
 }
 
 void RedPinchScreen::Draw()
 {
-    Sprite::PreDraw();
-    sprite_->Draw();
+	Sprite::PreDraw();
+	sprite_->Draw();
 }
 
--- project/Source/Application/GameObject/UI/RedPinchScreen.h
+++ project/Source/Application/GameObject/UI/RedPinchScreen.h
@@ -5,19 +5,25 @@
 class RedPinchScreen
 {
 public:
-    RedPinchScreen();
-    void Init();
-    void Update();
-    void TimerUpdate();
-    void Draw();
-    void SetHitPtr(bool& isHitPtr) {
-        isHitPtr_ = &isHitPtr;
-    };
-    HPs* hpPtr_ = nullptr;
+	RedPinchScreen();
+	void Init();
+	void Update();
+	void TimerUpdate();
+	void Draw();
+	void SetHitPtr(bool& isHitPtr) {
+		isHitPtr_ = &isHitPtr;
+	};
+	HPs* hpPtr_ = nullptr;
 private:
-   
-    bool* isHitPtr_ = nullptr;
-    std::unique_ptr<Sprite> sprite_;
-    float timer_ = 0.0f;
+
+	bool* isHitPtr_ = nullptr;
+	std::unique_ptr<Sprite> sprite_;
+	float timer_ = 0.0f;
+
+	const float kFlashTime_ = 0.7f;
+	float flashTimer_;
+	const float kFlashMaxAlpha_ = 0.6f;
+	const float kFlashMinAlpha_ = 0.1f;
+	bool isFlashing_;
 };
 
