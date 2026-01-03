5ef537f63c8ea649bd497d122166975a4a257a4a
あ <k024g0101@g.neec.ac.jp>
2026-01-04 05:51:45 +0900

    ok

--- project/Source/Application/Scene/TitleScene.cpp
+++ project/Source/Application/Scene/TitleScene.cpp
@@ -7,6 +7,8 @@
 #include"Quaternion/Quaternion.h"
 #include"CoordinateTransform.h"
 #include"InputBind.h"
+#include"Window.h"
+
 TitleScene::TitleScene()
 {
     //タイトル画像 
@@ -21,6 +23,20 @@ TitleScene::TitleScene()
     exitButton_ = std::make_unique<Sprite>();
     exitButton_->Create(Texture::TEXTURE_HANDLE::BUTTON_EXIT, { 640.0f, 528.0f });
     exitButton_->SetAnchorPoint({ 0.5f, 0.5f });
+
+    for (auto& sprite : test3Spritea_) {
+        sprite = std::make_unique<Sprite>();
+        sprite->Create(Texture::TEXTURE_HANDLE::TEST3, { 0.0f, 360.0f });
+        sprite->SetSize({ static_cast<float>(Window::GetClientWidth()), static_cast<float>(Window::GetClientHeight()) *0.5f});
+        sprite->SetUVScale({ 0.5f,1.0f,1.0f });
+    }
+
+    for (int i = 0; i < test3Spritea_.size(); ++i) {
+        test3Spritea_[i]->SetUVTranslate({ 0.0f, 0.0f, 0.0f });
+        test3Spritea_[i]->SetPosition({ 0.0f, test3Spritea_[i]->GetSize().y * i });
+
+    }
+
 }
 
 TitleScene::~TitleScene()
@@ -29,13 +45,19 @@ TitleScene::~TitleScene()
 
 void TitleScene::Initialize()
 {
-   isHoverStartButton_ = false;
-   isHoverExitButton_ = false;
-    sceneChange_->Initialize();
-    sceneChange_->SetState(SceneChange::kFadeOut, 60);
-    Sound::bgmVolume_ = 0.5f;
-    Sound::StopAllSound();
 
+        for (int i = 0; i < test3Spritea_.size(); ++i) {
+            test3Spritea_[i]->SetUVTranslate({ 0.0f, 0.0f, 0.0f });
+        }
+
+        isHoverStartButton_ = false;
+        isHoverExitButton_ = false;
+        sceneChange_->Initialize();
+        sceneChange_->SetState(SceneChange::kFadeOut, 60);
+        Sound::bgmVolume_ = 0.5f;
+        Sound::StopAllSound();
+
+    
 }
 
 void TitleScene::Update()
@@ -44,6 +66,12 @@ void TitleScene::Update()
     startButton_->SetColor({ 1.0f,1.0f,1.0f,1.0f });
     exitButton_->SetColor({ 1.0f,1.0f,1.0f,1.0f });
 
+
+    test3Spritea_[0]->GetUVTranslate().x += 0.001f;
+    test3Spritea_[1]->GetUVTranslate().x -= 0.001f;
+    for (auto& sprite : test3Spritea_) {
+        sprite->Update();
+    }
     Vector2 mousePos = Input::GetCursorPosition();
     if (IsCollision(mousePos, *startButton_)) {
         isHoverExitButton_ = false;
@@ -51,11 +79,11 @@ void TitleScene::Update()
         if (!isHoverStartButton_) {
             Sound::PlaySE(Sound::SWITCH_ON);
             isHoverStartButton_ = true;
-      
-        }
-      
-        if (InputBind::IsClick()) {
 
+        }
+
+        if (InputBind::IsClick()) {
+            Sound::PlaySE(Sound::FALL);
             sceneChange_->SetState(SceneChange::kFadeIn, 30);
             SceneManager::SetNestScene("Sample");
         }
@@ -68,9 +96,9 @@ void TitleScene::Update()
         if (!isHoverExitButton_) {
             Sound::PlaySE(Sound::SWITCH_ON);
             isHoverExitButton_ = true;
-         
+
         }
-   
+
         if (InputBind::IsClick()) {
             // アプリケーション終了 
             PostQuitMessage(0); // Windows APIでウィンドウを閉じる 
@@ -82,8 +110,11 @@ void TitleScene::Update()
 void TitleScene::Draw()
 {
     Sprite::PreDraw();
+    for (auto& sprite : test3Spritea_) {
+        sprite->Draw();
+    }
     titleSprite_->Draw();
-    startButton_->Draw(); 
+    startButton_->Draw();
     exitButton_->Draw();
 
     sceneChange_->Draw();
