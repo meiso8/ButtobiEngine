2cde7deb24bdc6e99d5cf081ae9de6987d762781
あ <k024g0101@g.neec.ac.jp>
2026-04-15 11:18:34 +0900

    SkyBoｘOK

--- project/Source/Application/Scene/FreeTypeScene.cpp
+++ project/Source/Application/Scene/FreeTypeScene.cpp
@@ -1,5 +1,7 @@
 #include "FreeTypeScene.h"
 #include"Input.h"
+#include"AABB.h"
+#include"DrawGrid.h"
 FreeTypeScene::FreeTypeScene()
 {
 
@@ -15,17 +17,28 @@ FreeTypeScene::FreeTypeScene()
     text_.SetBlendMode(BlendMode::kBlendModeAdd);
 
     pressSpaceText_.SetString(U"SPACE");
-    pressSpaceText_.SetPosition({ 640, 360+128 });
+    pressSpaceText_.SetPosition({ 640, 360 + 128 });
     pressSpaceText_.SetColor({ 1, 1, 1, 1 });
     pressSpaceText_.SetAlign(TextAlign::Center);
     pressSpaceText_.SetBlendMode(BlendMode::kBlendModeAdd);
     sprite_ = std::make_unique<Sprite>();
     sprite_->Create(TextureFactory::ART1, { 0,0 });
     sprite_->SetSize({ 1280,720 });
+
+
+    object3d_ = std::make_unique<Object3d>();
+    object3d_->Create();
+    skyBox_ = std::make_unique<SkyBox>();
+    skyBox_->Create(TextureFactory::UV_CHECKER);
+    object3d_->SetMesh(skyBox_.get());
+  
+ 
+
 }
 
 void FreeTypeScene::Initialize()
 {
+
 }
 
 void FreeTypeScene::Update()
@@ -52,14 +65,29 @@ void FreeTypeScene::Update()
         sceneChange_->SetState(SceneChange::kFadeIn, 30);
         SceneManager::SetNestScene("Title");
     }
+
+#ifdef _DEVELOP
+    // デバッグカメラの初期化
+    debugCamera_->UpdateMatrix();
+#endif //_DEVELOP
 }
 
 void FreeTypeScene::Draw()
 {
-    Sprite::PreDraw();
-    sprite_->Draw();
+
+
+
+#ifdef _DEVELOP
+    // デバッグカメラ
+    DrawGrid::Draw(*debugCamera_);
+    object3d_->Draw(*debugCamera_);
+#endif //_DEVELOP
+
+    //Sprite::PreDraw();
+    //sprite_->Draw();
     text_.Draw();
     pressSpaceText_.Draw();
 
     sceneChange_->Draw();
+
 }
