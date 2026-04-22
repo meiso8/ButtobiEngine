0b56ef20bc6c4ba2e6362b5e232a1e87acf3997f
あ <k024g0101@g.neec.ac.jp>
2026-04-22 14:37:32 +0900

    レベルエディタの追加

--- project/Source/level_editor.py
+++ project/Source/level_editor.py
@@ -1,4 +1,5 @@
 import bpy
+import math
 
 bl_info = {
     "name": "レベルエディタ",
@@ -39,6 +40,36 @@ class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
         bpy.ops.mesh.primitive_ico_sphere_add()
         print("ICO球を生成しました。")
         return {'FINISHED'}
+    
+class MYADDON_OT_export_scene(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_export_scene"
+    bl_label = "シーン出力"
+    bl_description = "シーン情報をExportします"
+
+    def execute(self,context):
+        print("シーン情報をExportします")
+        
+        for object in bpy.context.scene.objects:
+            print(object.type + " - " + object.name)
+            trans,rot, scale = object.matrix_local.decompose()
+            rot = rot.to_euler()
+            rot.x = math.degrees(rot.x)
+            rot.y = math.degrees(rot.y)
+            rot.z = math.degrees(rot.z)
+            print("Trans(%f,%f,%f)" % (trans.x,trans.y,trans.z))
+            print("Rot(%f,%f,%f)"% (rot.x,rot.y,rot.z))
+            print("Scale(%f,%f,%f)"% (scale.x,scale.y,scale.z))
+
+            if object.parent:
+                print("Parent:",object.parent.name)
+            print()
+
+        print("シーン情報をExportしました")
+        self.report({'INFO'},"シーン情報をExportしました")
+
+        return {'FINISHED'}
+
+
 
 class TOPBAR_MT_my_menu(bpy.types.Menu):
     bl_idname = "TOPBAR_MT_my_menu"
@@ -50,6 +81,8 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
         self.layout.separator()
         self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname,text = MYADDON_OT_create_ico_sphere.bl_label)
         self.layout.separator()
+        self.layout.operator(MYADDON_OT_export_scene.bl_idname,text = MYADDON_OT_export_scene.bl_label)
+        self.layout.separator()
         self.layout.operator("wm.url_open_preset",text ="Manual",icon ="HELP")
 
 
@@ -59,6 +92,7 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
 classes = (
     MYADDON_OT_stretch_vertex,
     MYADDON_OT_create_ico_sphere,
+    MYADDON_OT_export_scene,
     TOPBAR_MT_my_menu,
            )
 
