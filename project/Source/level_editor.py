935ee692c9331babf8c7abd6205a16b407a95aac
めいそ <k024g0101@g.neec.ac.jp>
2026-04-30 20:17:12 +0900

    レベルエディタでオブジェクト名の削除とfileNameの追加

--- project/Source/level_editor.py
+++ project/Source/level_editor.py
@@ -64,15 +64,19 @@ class MYADDON_OT_export_scene(bpy.types.Operator,bpy_extras.io_utils.ExportHelpe
         for i in range(level):
             indent += "\t"
 
-        self.write_and_print(file,indent + object.type + " - " + object.name)
+        self.write_and_print(file,indent + object.type)
         trans,rot, scale = object.matrix_local.decompose()
         rot = rot.to_euler()
         rot.x = math.degrees(rot.x)
         rot.y = math.degrees(rot.y)
         rot.z = math.degrees(rot.z)
-        self.write_and_print(file,indent + "Trans(%f,%f,%f)" % (trans.x,trans.y,trans.z))
-        self.write_and_print(file,indent + "Rot(%f,%f,%f)"% (rot.x,rot.y,rot.z))
-        self.write_and_print(file,indent + "Scale(%f,%f,%f)"% (scale.x,scale.y,scale.z))
+        self.write_and_print(file,indent + "T %f,%f,%f" % (trans.x,trans.y,trans.z))
+        self.write_and_print(file,indent + "R %f,%f,%f"% (rot.x,rot.y,rot.z))
+        self.write_and_print(file,indent + "S %f,%f,%f"% (scale.x,scale.y,scale.z))
+        #カスタムプロパティ'file_name'
+        if "file_name" in object:
+            self.write_and_print(file, indent + "N %s" % object["file_name"])
+        self.write_and_print(file, indent + 'END')
         self.write_and_print(file,'')
         
         #子ノードに進む
@@ -102,6 +106,35 @@ class MYADDON_OT_export_scene(bpy.types.Operator,bpy_extras.io_utils.ExportHelpe
         return {'FINISHED'}
     
 
+class MYADDON_OT_add_filename(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_add_filename"
+    bl_label = "FileName 追加"
+    bl_description = "['file_name']カスタムプロパティを追加します"
+    bl_options = {"REGISTER","UNDO"}
+
+    def execute(self,context):  
+        context.object["file_name"] = ""
+        return {"FINISHED"}
+
+class OBJECT_PT_file_name(bpy.types.Panel):
+    """オブジェクトのファイルネームパネル"""
+    bl_idname = "OBJECT_PT_file_name"
+    bl_label = "FileName"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "object"
+
+    # サブメニューの描画
+    def draw(self,context):
+        #パネルに項目を追加
+        if "file_name" in context.object:
+            #既にプロパティがあれば、プロパティを表示
+            self.layout.prop(context.object,'["file_name"]',text=self.bl_label)
+        else:
+            self.layout.operator(MYADDON_OT_add_filename.bl_idname)
+
+
+
 
 
 class TOPBAR_MT_my_menu(bpy.types.Menu):
@@ -127,6 +160,9 @@ classes = (
     MYADDON_OT_create_ico_sphere,
     MYADDON_OT_export_scene,
     TOPBAR_MT_my_menu,
+    MYADDON_OT_add_filename,
+    OBJECT_PT_file_name,
+
            )
 
 
